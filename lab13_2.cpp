528a5197c74ac2f2ebbdfb0569e490baff026f57
pattarapon <honamiyue@gmail.com>
2024-01-04 19:36:23 +0700

    complete

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,24 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+void showImage(const bool image[][M]) {
+    for (int i = 0; i < M + 2; i++) cout << "-";
+    cout << "\n";
+    for (int i = 0; i < N; i++) {
+        cout << "|";
+        for (int j = 0; j < M; j++) {
+            if (image[i][j]) cout << "*";
+            else cout << " ";
+        }
+        cout << "|\n";
+    }
+    for (int i = 0; i < M + 2; i++) cout << "-";
+    cout << "\n";
+}
+
+void updateImage(bool image[][M], int s, int x, int y) {
+    for (int i = max(0, x - s); i < min(N, x + s + 1); i++)
+        for (int j = max(0, y - s); j < min(M, y + s + 1); j++)
+            if (sqrt(pow(i - x, 2) + pow(j - y, 2)) <= s - 1)
+                image[i][j] = 1;
+}
